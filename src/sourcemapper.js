a2a0fb24636f9b2778b4552d8344ac04a29a6898
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-29 11:39:52 -0300

    [ :sparkles: ] | create: SourceMapper - handle > declaration (app)

--- src/sourcemapper.js
+++ src/sourcemapper.js
@@ -9,4 +9,16 @@ export default class SourceMapper {
     this.#minifiedLocalFilePath = minifiedLocalFilePath;
     this.#sourceMaps = new SourceMapGenerator({ file: minifiedLocalFilePath });
   }
+
+  #handleDeclaration({ loc: { start }, name }) {
+    if (this.#minifiedItems.has(name)) {
+      const nameMap = this.#minifiedItems.get(name);
+      nameMap.positions.push(start);
+
+      this.#minifiedItems.set(name, nameMap);
+      return;
+    }
+
+    this.#minifiedItems.set(name, { positions: [start] });
+  }
 }

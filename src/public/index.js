af8736eef2e945bd8c3152b3c4250bc599309b42
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-29 09:43:26 -0300

    [ :sparkles: ] | create: index (app::public)

---
+++ src/public/index.js
@@ -0,0 +1,12 @@
+function sum(var1, var2) {
+  return var1 + var2;
+}
+
+function info(name) {
+  const age = sum(8, 12);
+  const profession = "Software Engineer";
+  return `My name is ${name}, I am ${age} years old and I am a ${profession}`;
+}
+
+info("Hello World! :D");
+info("Samuel Ricardo");

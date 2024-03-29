4988b660a24d3cc95c5969183e733eea7cdb7af5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-03-29 10:28:15 -0300

    [ :sparkles: ] | setup: minifier - generate > name (app)

--- src/minifier.js
+++ src/minifier.js
@@ -1,4 +1,13 @@
 export default class Minifier {
   #nameMap = new Map();
   #alphabet = Array.from("abcdefghijklmnopqrstuvwxyz");
+
+  #generateNameIfNotExisting(name) {
+    if (this.#nameMap.has(name)) return this.#nameMap.get(name);
+
+    if (!this.#alphabet)
+      throw new Error("No more names available - limit is 52 tokens");
+
+    return this.#alphabet.shift();
+  }
 }
